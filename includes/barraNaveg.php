1011f20e482630cb2e00bcf1a5b5c9ee8e5a4e22
barauna <baraunasvinicius@gmail.com>
2020-05-27 21:08:29 -0300

    ProjetoFinal NETFLIX

---
+++ includes/barraNaveg.php
@@ -0,0 +1,22 @@
+<div class="barraTopo">
+    <div class="conteudoLogo">
+        <a href="index.php">
+            <img src="assets/images/web.png" alt="Logo">
+        </a>                
+    </div>
+
+    <ul class="linkNaveg">
+        <li><a href="index.php">Início</a></li>
+        <li><a href="series.php">Séries</a></li>
+        <li><a href="filmes.php">Filmes</a></li>
+    </ul>
+
+    <div class="itensDireita">
+        <a href="buscar.php">
+            <i class="fas fa-search"></i>
+        </a>
+        <a href="perfil.php">
+            <i class="fas fa-user"></i>
+        </a>
+    </div>
+</div>
